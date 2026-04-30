3bd90b2855093ae103b218c38dec7562637549f1
陈某 <54258201+gududaonet@users.noreply.github.com>
2026-04-30 21:41:42 +0800

    Upgrade Nodelinks code to version 2.0 with enhancements
    Updated Nodelinks to version 2.0 with new features and improved error handling.

--- Nodelinks-Workers.js
+++ Nodelinks-Workers.js
@@ -1,11 +1,23 @@
-// Nodelinks By GududaoStudio ver 0.0.1
-// Release 2025/12/20
+// Nodelinks By GududaoStudio ver 2.0 (Final Version)
+// Release 2026/04/30
 // Use in Cloudflare Workers
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
   })
   
-  const PASSWORD = 'yourpassword' // password setting
+  const PASSWORD = 'yourkey' // password setting
+  
+	function base64ToText(base64String) {
+		try {
+			const binaryString = atob(base64String);
+			const textDecoder = new TextDecoder('utf-8');
+			return textDecoder.decode(new TextEncoder().encode(binaryString));
+		} catch (error) {
+			return 'vless://66666666-6666-6666-6666-666666666666@1.1.1.1:166?fp=random&host=cloudflare.com&path=%2F&security=tls&sni=cloudflare.com&type=ws#DecodeError';
+
+		}
+	}
+
   
   async function handleRequest(request) {
     const url = new URL(request.url)
@@ -43,19 +55,31 @@ addEventListener('fetch', event => {
       let content = ''
   
       if (mode === '1') {
-        // mode1:safe mode(only for nodes link like "vmess://" in each line)
-        const res = await fetch('https://example.com/nodes/getnodes.txt')
-        const text0 = 'vmess://your-own-link-1'
-        const text1 = 'vmess://your-own-link-2'
+        // You can collect RAW nodes
+        const res = await fetch('https://example.com/nodes/rawnodes.txt')
+        const text0 = 'protocol://your-own-link-1'
+        const text1 = 'protocol://your-own-link-2'
         const text2 = await res.text()
         content = text0 + '\n'+ text1 + '\n' + text2
   
       } else if (mode === '2') {
-        // mode2: third-part service
-        const res = await fetch('https://example.com/nodes?link1=xxx&link2=xxx')
-        const text0 = await res.text()
-        content = text0
+        // Also can get ENCRYPTED nodes
+        const res = await fetch('https://example.com/nodes/encryptednodes.txt')
+        const text0 = 'protocol://your-own-link-1'
+        const text1 = 'protocol://your-own-link-2'
+        const text2 = await res.text()
+        content = text0 + '\n'+ text1 + '\n' + text2
   
+      } else if (mode === '3') {
+        // Mixed one is allowed
+        const res0 = await fetch('https://example.com/nodes/encryptednodes.txt')
+        const text2 = await res0.text()
+        const res1 = await fetch('https://example.com/nodes/encryptednodes.txt')
+        const text3 = await res1.text()
+        const text0 = 'protocol://your-own-link-1'
+        const text1 = 'protocol://your-own-link-2'
+        content = text0 + '\n'+ text1 + '\n' + text2 + '\n' + text3
+
       } else {
         return new Response(
         '<!DOCTYPE html><html><body><p>ACCESS DENIED</p></body></html>',
